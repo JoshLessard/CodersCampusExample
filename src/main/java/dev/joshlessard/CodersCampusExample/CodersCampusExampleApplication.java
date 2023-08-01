d9f36203294a695f8787f535ecdaec61850a014a
Josh Lessard <jlessard@axonify.com>
2023-07-31 21:54:39 -0400

    Spring Initializr application out-of-the-box.

---
+++ src/main/java/dev/joshlessard/CodersCampusExample/CodersCampusExampleApplication.java
@@ -0,0 +1,13 @@
+package dev.joshlessard.CodersCampusExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodersCampusExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodersCampusExampleApplication.class, args);
+	}
+
+}
