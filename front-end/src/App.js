29dd7fcdab32432db9b4c38385d9240e640c38d3
Josh Lessard <jlessard@axonify.com>
2023-08-19 23:16:51 -0400

    Create private routes (protected by login).

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -5,27 +5,29 @@ import { useLocalState } from './util/useLocalStorage';
 import { Route, Routes } from 'react-router-dom';
 import Dashboard from './Dashboard';
 import Homepage from './Homepage';
+import Login from './Login';
+import PrivateRoute from './PrivateRoute';
 
 function App() {
 
   const [jwt, setJwt] = useLocalState( "", "jwt" )
 
-  useEffect( () => {
-    if ( ! jwt ) {
-      fetch( "api/auth/login", {
-        headers: {
-           "Content-Type": "application/json"
-        },
-        method: "POST",
-        body: JSON.stringify( {
-          "username": "josh",
-          "password": "asdfasdf" 
-        } )
-      } )
-        .then( response => setJwt( response.headers.get( "authorization" ) ) )
-    }
-  },
-  [] )
+  // useEffect( () => {
+  //   if ( ! jwt ) {
+  //     fetch( "api/auth/login", {
+  //       headers: {
+  //          "Content-Type": "application/json"
+  //       },
+  //       method: "POST",
+  //       body: JSON.stringify( {
+  //         "username": "josh",
+  //         "password": "asdfasdf" 
+  //       } )
+  //     } )
+  //       .then( response => setJwt( response.headers.get( "authorization" ) ) )
+  //   }
+  // },
+  // [] )
 
   useEffect( () => {
     console.log( `JWT is: ${jwt}` )
@@ -33,7 +35,15 @@ function App() {
 
   return (
     <Routes>
-      <Route path="/dashboard" element={<Dashboard />} />
+      <Route
+        path="/dashboard"
+        element={
+          <PrivateRoute>
+            <Dashboard />
+          </PrivateRoute>
+        }
+      />
+      <Route path="/login" element={<Login/>} />
       <Route path="/" element={<Homepage />} />
     </Routes>
   )
---
+++ front-end/src/Login/index.js
@@ -0,0 +1,16 @@
+const Login = () => {
+    return (
+        <>
+            <div>
+                <label htmlFor="username">Username</label>
+                <input type="email" id="username" />
+            </div>
+            <div>
+                <label htmlFor="password">Password</label>
+                <input type="password" id="password" />
+            </div>
+        </>
+    )
+}
+
+export default Login;
