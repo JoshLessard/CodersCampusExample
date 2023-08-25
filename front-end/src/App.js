0486bc962dbc1f6a2e949b4f16a40821863b56c3
Josh Lessard <jlessard@axonify.com>
2023-08-24 23:33:42 -0400

    Implement a real login API call.

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -9,30 +9,6 @@ import Login from './Login';
 import PrivateRoute from './PrivateRoute';
 
 function App() {
-
-  const [jwt, setJwt] = useLocalState( "", "jwt" )
-
-  // useEffect( () => {
-  //   if ( ! jwt ) {
-  //     fetch( "api/auth/login", {
-  //       headers: {
-  //          "Content-Type": "application/json"
-  //       },
-  //       method: "POST",
-  //       body: JSON.stringify( {
-  //         "username": "josh",
-  //         "password": "asdfasdf" 
-  //       } )
-  //     } )
-  //       .then( response => setJwt( response.headers.get( "authorization" ) ) )
-  //   }
-  // },
-  // [] )
-
-  useEffect( () => {
-    console.log( `JWT is: ${jwt}` )
-  }, [jwt] )
-
   return (
     <Routes>
       <Route
