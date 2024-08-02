190c7c191d7105ab720ffa7fe8fee9145c0fd82b
Josh Lessard <jlessard@axonify.com>
2024-08-02 18:38:36 -0400

    Lesson 21: Using Flexbox in Bootstrap and Centering Elements

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -8,7 +8,7 @@ import Homepage from './Homepage';
 import Login from './Login';
 import PrivateRoute from './PrivateRoute';
 import AssignmentView from './AssignmentView';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
--- front-end/src/Login/index.js
+++ front-end/src/Login/index.js
@@ -38,36 +38,49 @@ const Login = () => {
     return (
         <>
             <Container className="mt-5">
-                <Form.Group className="mb-3">
-                    <Form.Label htmlFor="username" className="fs-4">Username</Form.Label>
-                    <Form.Control
-                        type="email"
-                        id="username"
-                        placeholder="Enter your email address"
-                        value={username}
-                        onChange={e => setUsername(e.target.value)}
-                    />
-                </Form.Group>
-                <Form.Group className="mb-3">
-                    <Form.Label htmlFor="password" className="fs-4">Password</Form.Label>
-                    <Form.Control
-                        type="password"
-                        id="password"
-                        value={password}
-                        onChange={e => setPassword(e.target.value)}
-                    />
-                </Form.Group>
-                <Row>
-                    <Col className="mt-2">
-                        <div>
-                            <Button
-                                id="submit"
-                                type="button"
-                                onClick={() => sendLoginRequest()}
-                            >
-                                Log In
-                            </Button>
-                        </div>
+                <Row className="justify-content-center">
+                    <Col md="8" lg="6">
+                        <Form.Group className="mb-3" controlId="username">
+                            <Form.Label className="fs-4">Username</Form.Label>
+                            <Form.Control
+                                type="email"
+                                placeholder="Enter your email address"
+                                value={username}
+                                onChange={e => setUsername(e.target.value)}
+                            />
+                        </Form.Group>
+                    </Col>
+                </Row>
+                <Row className="justify-content-center">
+                    <Col md="8" lg="6">
+                        <Form.Group className="mb-3 " controlId="password">
+                            <Form.Label className="fs-4">Password</Form.Label>
+                            <Form.Control
+                                type="password"
+                                value={password}
+                                onChange={e => setPassword(e.target.value)}
+                            />
+                        </Form.Group>
+                    </Col>
+                </Row>
+                <Row className="justify-content-center">
+                    <Col md="8" lg="6" className="mt-2 d-flex flex-column gap-5 flex-md-row justify-content-md-between">
+                        <Button
+                            id="submit"
+                            type="button"
+                            size="lg"
+                            onClick={() => sendLoginRequest()}
+                        >
+                            Log In
+                        </Button>
+                        <Button
+                            variant="secondary"
+                            type="button"
+                            size="lg"
+                            onClick={() => {window.location.href="/"}}
+                        >
+                            Exit
+                        </Button>
                     </Col>
                 </Row>
             </Container>
